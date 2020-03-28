e10083abb82f1b681a07de95e815f9106c59f3a9
amontecillo <amontecillo@marqeta.com>
2020-03-28 16:21:21 -0700

    Reformat code

--- src/main/java/io/grpc/examples/KvClient.java
+++ src/main/java/io/grpc/examples/KvClient.java
@@ -10,15 +10,9 @@ import io.grpc.Channel;
 import io.grpc.ClientCall;
 import io.grpc.Status;
 import io.grpc.Status.Code;
-import io.grpc.examples.KvJava.CreateRequest;
-import io.grpc.examples.KvJava.CreateResponse;
-import io.grpc.examples.KvJava.DeleteRequest;
-import io.grpc.examples.KvJava.DeleteResponse;
-import io.grpc.examples.KvJava.RetrieveRequest;
-import io.grpc.examples.KvJava.RetrieveResponse;
-import io.grpc.examples.KvJava.UpdateRequest;
-import io.grpc.examples.KvJava.UpdateResponse;
+import io.grpc.examples.KvJava.*;
 import io.grpc.stub.ClientCalls;
+
 import java.nio.ByteBuffer;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
@@ -33,254 +27,253 @@ import java.util.logging.Logger;
  * and deleting them.  A real program would actually use the values, and they wouldn't be random.
  */
 final class KvClient {
-  private static final Logger logger = Logger.getLogger(KvClient.class.getName());
+    private static final Logger logger = Logger.getLogger(KvClient.class.getName());
 
-  private final int MEAN_KEY_SIZE = 64;
-  private final int MEAN_VALUE_SIZE = 65536;
+    private final int MEAN_KEY_SIZE = 64;
+    private final int MEAN_VALUE_SIZE = 65536;
 
-  private final RandomAccessSet<ByteBuffer> knownKeys = new RandomAccessSet<>();
-  private final Channel channel;
+    private final RandomAccessSet<ByteBuffer> knownKeys = new RandomAccessSet<>();
+    private final Channel channel;
+    private final Semaphore limiter = new Semaphore(100);
+    private AtomicLong rpcCount = new AtomicLong();
 
-  private AtomicLong rpcCount = new AtomicLong();
-  private final Semaphore limiter = new Semaphore(100);
-
-  KvClient(Channel channel) {
-    this.channel = channel;
-  }
-
-  long getRpcCount() {
-    return rpcCount.get();
-  }
-
-  /**
-   * Does the client work until {@code done.get()} returns true.  Callers should set done to true,
-   * and wait for this method to return.
-   */
-  void doClientWork(AtomicBoolean done) throws InterruptedException {
-    Random random = new Random();
-    AtomicReference<Throwable> errors = new AtomicReference<>();
-
-    while (!done.get() && errors.get() == null) {
-      // Pick a random CRUD action to take.
-      int command = random.nextInt(4);
-      if (command == 0) {
-        doCreate(channel, errors);
-      } else if (command == 1) {
-        doRetrieve(channel, errors);
-      } else if (command == 2) {
-        doUpdate(channel, errors);
-      } else if (command == 3) {
-        doDelete(channel, errors);
-      } else {
-        throw new AssertionError();
-      }
+    KvClient(Channel channel) {
+        this.channel = channel;
     }
-    if (errors.get() != null) {
-      throw new RuntimeException(errors.get());
+
+    /**
+     * Creates an exponentially sized byte string with a mean size.
+     */
+    private static ByteBuffer randomBytes(int mean) {
+        Random random = new Random();
+        // An exponentially distributed random number.
+        int size = (int) Math.round(mean * -Math.log(1 - random.nextDouble()));
+        byte[] bytes = new byte[1 + size];
+        random.nextBytes(bytes);
+        return ByteBuffer.wrap(bytes);
     }
-  }
 
-  /**
-   * Creates a random key and value.
-   */
-  private void doCreate(Channel chan, AtomicReference<Throwable> error)
-      throws InterruptedException {
-    limiter.acquire();
-    ByteBuffer key = createRandomKey();
-    ClientCall<CreateRequest, CreateResponse> call =
-        chan.newCall(KvJava.CREATE_METHOD, CallOptions.DEFAULT);
-    KvJava.CreateRequest req = new KvJava.CreateRequest();
-    req.key = key.array();
-    req.value = randomBytes(MEAN_VALUE_SIZE).array();
+    long getRpcCount() {
+        return rpcCount.get();
+    }
 
-    ListenableFuture<CreateResponse> res = ClientCalls.futureUnaryCall(call, req);
-    res.addListener(() ->  {
-      rpcCount.incrementAndGet();
-      limiter.release();
-    }, MoreExecutors.directExecutor());
-    Futures.addCallback(res, new FutureCallback<CreateResponse>() {
-      @Override
-      public void onSuccess(CreateResponse result) {
+    /**
+     * Does the client work until {@code done.get()} returns true.  Callers should set done to true,
+     * and wait for this method to return.
+     */
+    void doClientWork(AtomicBoolean done) throws InterruptedException {
+        Random random = new Random();
+        AtomicReference<Throwable> errors = new AtomicReference<>();
+
+        while (!done.get() && errors.get() == null) {
+            // Pick a random CRUD action to take.
+            int command = random.nextInt(4);
+            if (command == 0) {
+                doCreate(channel, errors);
+            } else if (command == 1) {
+                doRetrieve(channel, errors);
+            } else if (command == 2) {
+                doUpdate(channel, errors);
+            } else if (command == 3) {
+                doDelete(channel, errors);
+            } else {
+                throw new AssertionError();
+            }
+        }
+        if (errors.get() != null) {
+            throw new RuntimeException(errors.get());
+        }
+    }
+
+    /**
+     * Creates a random key and value.
+     */
+    private void doCreate(Channel chan, AtomicReference<Throwable> error)
+            throws InterruptedException {
+        limiter.acquire();
+        ByteBuffer key = createRandomKey();
+        ClientCall<CreateRequest, CreateResponse> call =
+                chan.newCall(KvJava.CREATE_METHOD, CallOptions.DEFAULT);
+        KvJava.CreateRequest req = new KvJava.CreateRequest();
+        req.key = key.array();
+        req.value = randomBytes(MEAN_VALUE_SIZE).array();
+
+        ListenableFuture<CreateResponse> res = ClientCalls.futureUnaryCall(call, req);
+        res.addListener(() -> {
+            rpcCount.incrementAndGet();
+            limiter.release();
+        }, MoreExecutors.directExecutor());
+        Futures.addCallback(res, new FutureCallback<CreateResponse>() {
+            @Override
+            public void onSuccess(CreateResponse result) {
+                synchronized (knownKeys) {
+                    knownKeys.add(key);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                if (status.getCode() == Code.ALREADY_EXISTS) {
+                    synchronized (knownKeys) {
+                        knownKeys.remove(key);
+                    }
+                    logger.log(Level.INFO, "Key already existed", t);
+                } else {
+                    error.compareAndSet(null, t);
+                }
+            }
+        });
+    }
+
+    /**
+     * Retrieves the value of a random key.
+     */
+    private void doRetrieve(Channel chan, AtomicReference<Throwable> error)
+            throws InterruptedException {
+
+        ByteBuffer key;
         synchronized (knownKeys) {
-          knownKeys.add(key);
+            key = knownKeys.getRandomKey();
         }
-      }
-
-      @Override
-      public void onFailure(Throwable t) {
-        Status status = Status.fromThrowable(t);
-        if (status.getCode() == Code.ALREADY_EXISTS) {
-          synchronized (knownKeys) {
-            knownKeys.remove(key);
-          }
-          logger.log(Level.INFO, "Key already existed", t);
-        } else {
-          error.compareAndSet(null, t);
+        if (key == null) {
+            logger.log(Level.FINE, "Nothing to retrieve, continue with the next random action.");
+            return;
         }
-      }
-    });
-  }
 
-  /**
-   * Retrieves the value of a random key.
-   */
-  private void doRetrieve(Channel chan, AtomicReference<Throwable> error)
-      throws InterruptedException {
+        limiter.acquire();
+        ClientCall<RetrieveRequest, RetrieveResponse> call =
+                chan.newCall(KvJava.RETRIEVE_METHOD, CallOptions.DEFAULT);
+        KvJava.RetrieveRequest req = new KvJava.RetrieveRequest();
+        req.key = key.array();
+        ListenableFuture<RetrieveResponse> res = ClientCalls.futureUnaryCall(call, req);
+        res.addListener(() -> {
+            rpcCount.incrementAndGet();
+            limiter.release();
+        }, MoreExecutors.directExecutor());
+        Futures.addCallback(res, new FutureCallback<RetrieveResponse>() {
+            @Override
+            public void onSuccess(RetrieveResponse result) {
+                if (result.value.length < 1) {
+                    error.compareAndSet(null, new RuntimeException("Invalid response"));
+                }
+            }
 
-    ByteBuffer key;
-    synchronized (knownKeys) {
-      key = knownKeys.getRandomKey();
-    }
-    if (key == null) {
-      logger.log(Level.FINE, "Nothing to retrieve, continue with the next random action.");
-      return;
-    }
-
-    limiter.acquire();
-    ClientCall<RetrieveRequest, RetrieveResponse> call =
-        chan.newCall(KvJava.RETRIEVE_METHOD, CallOptions.DEFAULT);
-    KvJava.RetrieveRequest req = new KvJava.RetrieveRequest();
-    req.key = key.array();
-    ListenableFuture<RetrieveResponse> res = ClientCalls.futureUnaryCall(call, req);
-    res.addListener(() ->  {
-      rpcCount.incrementAndGet();
-      limiter.release();
-    }, MoreExecutors.directExecutor());
-    Futures.addCallback(res, new FutureCallback<RetrieveResponse>() {
-      @Override
-      public void onSuccess(RetrieveResponse result) {
-        if (result.value.length < 1) {
-          error.compareAndSet(null, new RuntimeException("Invalid response"));
-        }
-      }
-
-      @Override
-      public void onFailure(Throwable t) {
-        Status status = Status.fromThrowable(t);
-        if (status.getCode() == Code.NOT_FOUND) {
-          synchronized (knownKeys) {
-            knownKeys.remove(key);
-          }
-          logger.log(Level.INFO, "Key not found", t);
-        } else {
-          error.compareAndSet(null, t);
-        }
-      }
-    });
-  }
-
-  /**
-   * Updates a random key with a random value.
-   */
-  private void doUpdate(Channel chan, AtomicReference<Throwable> error)
-      throws InterruptedException {
-
-    ByteBuffer key;
-    synchronized (knownKeys) {
-      key = knownKeys.getRandomKey();
-    }
-    if (key == null) {
-      logger.log(Level.FINE, "Nothing to update, continue with the next random action.");
-      return;
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                if (status.getCode() == Code.NOT_FOUND) {
+                    synchronized (knownKeys) {
+                        knownKeys.remove(key);
+                    }
+                    logger.log(Level.INFO, "Key not found", t);
+                } else {
+                    error.compareAndSet(null, t);
+                }
+            }
+        });
     }
 
-    limiter.acquire();
-    ClientCall<UpdateRequest, UpdateResponse> call =
-        channel.newCall(KvJava.UPDATE_METHOD, CallOptions.DEFAULT);
-    KvJava.UpdateRequest req = new KvJava.UpdateRequest();
-    req.key = key.array();
-    req.value = randomBytes(MEAN_VALUE_SIZE).array();
+    /**
+     * Updates a random key with a random value.
+     */
+    private void doUpdate(Channel chan, AtomicReference<Throwable> error)
+            throws InterruptedException {
 
-    ListenableFuture<UpdateResponse> res = ClientCalls.futureUnaryCall(call, req);
-    res.addListener(() ->  {
-      rpcCount.incrementAndGet();
-      limiter.release();
-    }, MoreExecutors.directExecutor());
-    Futures.addCallback(res, new FutureCallback<UpdateResponse>() {
-      @Override
-      public void onSuccess(UpdateResponse result) {
-      }
-
-      @Override
-      public void onFailure(Throwable t) {
-        Status status = Status.fromThrowable(t);
-        if (status.getCode() == Code.NOT_FOUND) {
-          synchronized (knownKeys) {
-            knownKeys.remove(key);
-          }
-          logger.log(Level.INFO, "Key not found", t);
-        } else {
-          error.compareAndSet(null, t);
+        ByteBuffer key;
+        synchronized (knownKeys) {
+            key = knownKeys.getRandomKey();
         }
-      }
-    });
-  }
+        if (key == null) {
+            logger.log(Level.FINE, "Nothing to update, continue with the next random action.");
+            return;
+        }
 
-  /**
-   * Deletes the value of a random key.
-   */
-  private void doDelete(Channel chan, AtomicReference<Throwable> error)
-      throws InterruptedException {
+        limiter.acquire();
+        ClientCall<UpdateRequest, UpdateResponse> call =
+                channel.newCall(KvJava.UPDATE_METHOD, CallOptions.DEFAULT);
+        KvJava.UpdateRequest req = new KvJava.UpdateRequest();
+        req.key = key.array();
+        req.value = randomBytes(MEAN_VALUE_SIZE).array();
 
-    ByteBuffer key;
-    synchronized (knownKeys) {
-      key = knownKeys.getRandomKey();
-      if (key != null) {
-        knownKeys.remove(key);
-      }
-    }
-    if (key == null) { // log outside of a synchronized block
-      logger.log(Level.FINE, "Nothing to delete, continue with the next random action.");
-      return;
+        ListenableFuture<UpdateResponse> res = ClientCalls.futureUnaryCall(call, req);
+        res.addListener(() -> {
+            rpcCount.incrementAndGet();
+            limiter.release();
+        }, MoreExecutors.directExecutor());
+        Futures.addCallback(res, new FutureCallback<UpdateResponse>() {
+            @Override
+            public void onSuccess(UpdateResponse result) {
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                if (status.getCode() == Code.NOT_FOUND) {
+                    synchronized (knownKeys) {
+                        knownKeys.remove(key);
+                    }
+                    logger.log(Level.INFO, "Key not found", t);
+                } else {
+                    error.compareAndSet(null, t);
+                }
+            }
+        });
     }
 
-    limiter.acquire();
-    ClientCall<DeleteRequest, DeleteResponse> call =
-        chan.newCall(KvJava.DELETE_METHOD, CallOptions.DEFAULT);
-    DeleteRequest req = new DeleteRequest();
-    req.key = key.array();
-    ListenableFuture<DeleteResponse> res = ClientCalls.futureUnaryCall(call, req);
-    res.addListener(() ->  {
-      rpcCount.incrementAndGet();
-      limiter.release();
-    }, MoreExecutors.directExecutor());
-    Futures.addCallback(res, new FutureCallback<DeleteResponse>() {
-      @Override
-      public void onSuccess(DeleteResponse result) {
-      }
+    /**
+     * Deletes the value of a random key.
+     */
+    private void doDelete(Channel chan, AtomicReference<Throwable> error)
+            throws InterruptedException {
 
-      @Override
-      public void onFailure(Throwable t) {
-        Status status = Status.fromThrowable(t);
-        if (status.getCode() == Code.NOT_FOUND) {
-          logger.log(Level.INFO, "Key not found", t);
-        } else {
-          error.compareAndSet(null, t);
+        ByteBuffer key;
+        synchronized (knownKeys) {
+            key = knownKeys.getRandomKey();
+            if (key != null) {
+                knownKeys.remove(key);
+            }
         }
-      }
-    });
-  }
+        if (key == null) { // log outside of a synchronized block
+            logger.log(Level.FINE, "Nothing to delete, continue with the next random action.");
+            return;
+        }
 
-  /**
-   * Creates and adds a key to the set of known keys.
-   */
-  private ByteBuffer createRandomKey() {
-    ByteBuffer key;
-    do {
-      key = randomBytes(MEAN_KEY_SIZE);
-    } while (knownKeys.contains(key));
-    return key;
-  }
+        limiter.acquire();
+        ClientCall<DeleteRequest, DeleteResponse> call =
+                chan.newCall(KvJava.DELETE_METHOD, CallOptions.DEFAULT);
+        DeleteRequest req = new DeleteRequest();
+        req.key = key.array();
+        ListenableFuture<DeleteResponse> res = ClientCalls.futureUnaryCall(call, req);
+        res.addListener(() -> {
+            rpcCount.incrementAndGet();
+            limiter.release();
+        }, MoreExecutors.directExecutor());
+        Futures.addCallback(res, new FutureCallback<DeleteResponse>() {
+            @Override
+            public void onSuccess(DeleteResponse result) {
+            }
 
-  /**
-   * Creates an exponentially sized byte string with a mean size.
-   */
-  private static ByteBuffer randomBytes(int mean) {
-    Random random = new Random();
-    // An exponentially distributed random number.
-    int size = (int) Math.round(mean * -Math.log(1 - random.nextDouble()));
-    byte[] bytes = new byte[1 + size];
-    random.nextBytes(bytes);
-    return ByteBuffer.wrap(bytes);
-  }
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                if (status.getCode() == Code.NOT_FOUND) {
+                    logger.log(Level.INFO, "Key not found", t);
+                } else {
+                    error.compareAndSet(null, t);
+                }
+            }
+        });
+    }
+
+    /**
+     * Creates and adds a key to the set of known keys.
+     */
+    private ByteBuffer createRandomKey() {
+        ByteBuffer key;
+        do {
+            key = randomBytes(MEAN_KEY_SIZE);
+        } while (knownKeys.contains(key));
+        return key;
+    }
 }
--- src/main/java/io/grpc/examples/KvService.java
+++ src/main/java/io/grpc/examples/KvService.java
@@ -2,6 +2,7 @@ package io.grpc.examples;
 
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+
 import java.nio.ByteBuffer;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -15,75 +16,75 @@ import java.util.concurrent.TimeUnit;
  */
 final class KvService extends KvJava.KeyValueServiceImplBase {
 
-  private static final long READ_DELAY_MILLIS = 10;
-  private static final long WRITE_DELAY_MILLIS = 50;
+    private static final long READ_DELAY_MILLIS = 10;
+    private static final long WRITE_DELAY_MILLIS = 50;
 
-  private final ConcurrentMap<ByteBuffer, ByteBuffer> store = new ConcurrentHashMap<>();
+    private final ConcurrentMap<ByteBuffer, ByteBuffer> store = new ConcurrentHashMap<>();
 
-  @Override
-  public void create(
-      KvJava.CreateRequest request, StreamObserver<KvJava.CreateResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    ByteBuffer value = ByteBuffer.wrap(request.value);
-    simulateWork(WRITE_DELAY_MILLIS);
-    if (store.putIfAbsent(key, value) == null) {
-      responseObserver.onNext(new KvJava.CreateResponse());
-      responseObserver.onCompleted();
-      return;
+    @Override
+    public void create(
+            KvJava.CreateRequest request, StreamObserver<KvJava.CreateResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        ByteBuffer value = ByteBuffer.wrap(request.value);
+        simulateWork(WRITE_DELAY_MILLIS);
+        if (store.putIfAbsent(key, value) == null) {
+            responseObserver.onNext(new KvJava.CreateResponse());
+            responseObserver.onCompleted();
+            return;
+        }
+        responseObserver.onError(Status.ALREADY_EXISTS.asRuntimeException());
     }
-    responseObserver.onError(Status.ALREADY_EXISTS.asRuntimeException());
-  }
 
-  @Override
-  public void retrieve(KvJava.RetrieveRequest request,
-      StreamObserver<KvJava.RetrieveResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    simulateWork(READ_DELAY_MILLIS);
-    ByteBuffer value = store.get(key);
-    if (value != null) {
-      KvJava.RetrieveResponse response = new KvJava.RetrieveResponse();
-      response.value = value.array();
-      responseObserver.onNext(response);
-      responseObserver.onCompleted();
-      return;
-    }
-    responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
-  }
-
-  @Override
-  public void update(
-      KvJava.UpdateRequest request, StreamObserver<KvJava.UpdateResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    ByteBuffer newValue = ByteBuffer.wrap(request.value);
-    simulateWork(WRITE_DELAY_MILLIS);
-    ByteBuffer oldValue;
-    do {
-      oldValue = store.get(key);
-      if (oldValue == null) {
+    @Override
+    public void retrieve(KvJava.RetrieveRequest request,
+                         StreamObserver<KvJava.RetrieveResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        simulateWork(READ_DELAY_MILLIS);
+        ByteBuffer value = store.get(key);
+        if (value != null) {
+            KvJava.RetrieveResponse response = new KvJava.RetrieveResponse();
+            response.value = value.array();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            return;
+        }
         responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
-        return;
-      }
-    } while (!store.replace(key, oldValue, newValue));
-    responseObserver.onNext(new KvJava.UpdateResponse());
-    responseObserver.onCompleted();
-  }
-
-  @Override
-  public void delete(
-      KvJava.DeleteRequest request, StreamObserver<KvJava.DeleteResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    simulateWork(WRITE_DELAY_MILLIS);
-    store.remove(key);
-    responseObserver.onNext(new KvJava.DeleteResponse());
-    responseObserver.onCompleted();
-  }
-
-  private static void simulateWork(long millis) {
-    try {
-      TimeUnit.MILLISECONDS.sleep(millis);
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      throw new RuntimeException(e);
     }
-  }
+
+    @Override
+    public void update(
+            KvJava.UpdateRequest request, StreamObserver<KvJava.UpdateResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        ByteBuffer newValue = ByteBuffer.wrap(request.value);
+        simulateWork(WRITE_DELAY_MILLIS);
+        ByteBuffer oldValue;
+        do {
+            oldValue = store.get(key);
+            if (oldValue == null) {
+                responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+                return;
+            }
+        } while (!store.replace(key, oldValue, newValue));
+        responseObserver.onNext(new KvJava.UpdateResponse());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void delete(
+            KvJava.DeleteRequest request, StreamObserver<KvJava.DeleteResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        simulateWork(WRITE_DELAY_MILLIS);
+        store.remove(key);
+        responseObserver.onNext(new KvJava.DeleteResponse());
+        responseObserver.onCompleted();
+    }
+
+    private static void simulateWork(long millis) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
 }
