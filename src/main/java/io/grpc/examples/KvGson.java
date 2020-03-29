263403816d8215c1ade3e13a02f2012727e1b6b5
junkfactory <junkfactory@gmail.com>
2020-03-28 22:22:16 -0700

    Upgrade libraries

--- src/main/java/io/grpc/examples/KvGson.java
+++ src/main/java/io/grpc/examples/KvGson.java
@@ -12,6 +12,7 @@ import io.grpc.MethodDescriptor.MethodType;
 import io.grpc.ServerServiceDefinition;
 import io.grpc.stub.ServerCalls;
 import io.grpc.stub.StreamObserver;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,129 +22,126 @@ import java.util.Base64;
 
 final class KvGson {
 
-  private static final String SERVICE_NAME = "io.grpc.KeyValueService";
+    private static final String SERVICE_NAME = "io.grpc.KeyValueService";
+    private static final Gson gson =
+            new GsonBuilder().registerTypeAdapter(byte[].class, new TypeAdapter<byte[]>() {
+                @Override
+                public void write(JsonWriter out, byte[] value) throws IOException {
+                    out.value(Base64.getEncoder().encodeToString(value));
+                }
 
-  static final class CreateRequest {
-    byte[] key;
-    byte[] value;
-  }
+                @Override
+                public byte[] read(JsonReader in) throws IOException {
+                    return Base64.getDecoder().decode(in.nextString());
+                }
+            }).create();
+    static final MethodDescriptor<CreateRequest, CreateResponse> CREATE_METHOD =
+            MethodDescriptor.newBuilder(
+                    marshallerFor(CreateRequest.class),
+                    marshallerFor(CreateResponse.class))
+                    .setFullMethodName(
+                            MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Create"))
+                    .setType(MethodType.UNARY)
+                    .setSampledToLocalTracing(true)
+                    .build();
+    static final MethodDescriptor<RetrieveRequest, RetrieveResponse> RETRIEVE_METHOD =
+            MethodDescriptor.newBuilder(
+                    marshallerFor(RetrieveRequest.class),
+                    marshallerFor(RetrieveResponse.class))
+                    .setFullMethodName(
+                            MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Retrieve"))
+                    .setType(MethodType.UNARY)
+                    .setSampledToLocalTracing(true)
+                    .build();
+    static final MethodDescriptor<UpdateRequest, UpdateResponse> UPDATE_METHOD =
+            MethodDescriptor.newBuilder(
+                    marshallerFor(UpdateRequest.class),
+                    marshallerFor(UpdateResponse.class))
+                    .setFullMethodName(
+                            MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Update"))
+                    .setType(MethodType.UNARY)
+                    .setSampledToLocalTracing(true)
+                    .build();
+    static final MethodDescriptor<DeleteRequest, DeleteResponse> DELETE_METHOD =
+            MethodDescriptor.newBuilder(
+                    marshallerFor(DeleteRequest.class),
+                    marshallerFor(DeleteResponse.class))
+                    .setFullMethodName(
+                            MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Delete"))
+                    .setType(MethodType.UNARY)
+                    .setSampledToLocalTracing(true)
+                    .build();
 
-  static final class CreateResponse{}
+    static <T> Marshaller<T> marshallerFor(Class<T> clz) {
+        return new Marshaller<T>() {
+            @Override
+            public InputStream stream(T value) {
+                return new ByteArrayInputStream(gson.toJson(value, clz).getBytes(StandardCharsets.UTF_8));
+            }
 
-  static final MethodDescriptor<CreateRequest, CreateResponse> CREATE_METHOD =
-      MethodDescriptor.newBuilder(
-          marshallerFor(CreateRequest.class),
-          marshallerFor(CreateResponse.class))
-          .setFullMethodName(
-              MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Create"))
-          .setType(MethodType.UNARY)
-          .setSampledToLocalTracing(true)
-          .build();
-
-  static final class RetrieveRequest {
-    byte[] key;
-  }
-
-  static final class RetrieveResponse {
-    byte[] value;
-  }
-
-  static final MethodDescriptor<RetrieveRequest, RetrieveResponse> RETRIEVE_METHOD =
-      MethodDescriptor.newBuilder(
-          marshallerFor(RetrieveRequest.class),
-          marshallerFor(RetrieveResponse.class))
-          .setFullMethodName(
-              MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Retrieve"))
-          .setType(MethodType.UNARY)
-          .setSampledToLocalTracing(true)
-          .build();
-
-  static final class UpdateRequest {
-    byte[] key;
-    byte[] value;
-  }
-
-  static final class UpdateResponse {}
-
-  static final MethodDescriptor<UpdateRequest, UpdateResponse> UPDATE_METHOD =
-      MethodDescriptor.newBuilder(
-          marshallerFor(UpdateRequest.class),
-          marshallerFor(UpdateResponse.class))
-          .setFullMethodName(
-              MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Update"))
-          .setType(MethodType.UNARY)
-          .setSampledToLocalTracing(true)
-          .build();
-
-
-  static final class DeleteRequest {
-    byte[] key;
-  }
-
-  static final class DeleteResponse {}
-
-  static final MethodDescriptor<DeleteRequest, DeleteResponse> DELETE_METHOD =
-      MethodDescriptor.newBuilder(
-          marshallerFor(DeleteRequest.class),
-          marshallerFor(DeleteResponse.class))
-          .setFullMethodName(
-              MethodDescriptor.generateFullMethodName(SERVICE_NAME, "Delete"))
-          .setType(MethodType.UNARY)
-          .setSampledToLocalTracing(true)
-          .build();
-
-  static abstract class KeyValueServiceImplBase implements BindableService {
-    public abstract void create(
-        KvGson.CreateRequest request, StreamObserver<CreateResponse> responseObserver);
-
-    public abstract void retrieve(KvGson.RetrieveRequest request,
-        StreamObserver<KvGson.RetrieveResponse> responseObserver);
-
-    public abstract void update(
-        KvGson.UpdateRequest request, StreamObserver<KvGson.UpdateResponse> responseObserver);
-
-    public abstract void delete(
-        KvGson.DeleteRequest request, StreamObserver<KvGson.DeleteResponse> responseObserver);
-
-    @Override
-    public final ServerServiceDefinition bindService() {
-      ServerServiceDefinition.Builder ssd = ServerServiceDefinition.builder(SERVICE_NAME);
-      ssd.addMethod(CREATE_METHOD, ServerCalls.asyncUnaryCall(
-          (request, responseObserver) -> create(request, responseObserver)));
-      ssd.addMethod(RETRIEVE_METHOD, ServerCalls.asyncUnaryCall(
-          (request, responseObserver) -> retrieve(request, responseObserver)));
-      ssd.addMethod(UPDATE_METHOD, ServerCalls.asyncUnaryCall(
-          (request, responseObserver) -> update(request, responseObserver)));
-      ssd.addMethod(DELETE_METHOD, ServerCalls.asyncUnaryCall(
-          (request, responseObserver) -> delete(request, responseObserver)));
-      return ssd.build();
-    }
-  }
-
-  private static final Gson gson =
-      new GsonBuilder().registerTypeAdapter(byte[].class, new TypeAdapter<byte[]>() {
-    @Override
-    public void write(JsonWriter out, byte[] value) throws IOException {
-      out.value(Base64.getEncoder().encodeToString(value));
+            @Override
+            public T parse(InputStream stream) {
+                return gson.fromJson(new InputStreamReader(stream, StandardCharsets.UTF_8), clz);
+            }
+        };
     }
 
-    @Override
-    public byte[] read(JsonReader in) throws IOException {
-      return Base64.getDecoder().decode(in.nextString());
+    static final class CreateRequest {
+        byte[] key;
+        byte[] value;
     }
-  }).create();
 
-  static <T> Marshaller<T> marshallerFor(Class<T> clz) {
-    return new Marshaller<T>() {
-      @Override
-      public InputStream stream(T value) {
-        return new ByteArrayInputStream(gson.toJson(value, clz).getBytes(StandardCharsets.UTF_8));
-      }
+    static final class CreateResponse {
+    }
 
-      @Override
-      public T parse(InputStream stream) {
-        return gson.fromJson(new InputStreamReader(stream, StandardCharsets.UTF_8), clz);
-      }
-    };
-  }
+    static final class RetrieveRequest {
+        byte[] key;
+    }
+
+    static final class RetrieveResponse {
+        byte[] value;
+    }
+
+    static final class UpdateRequest {
+        byte[] key;
+        byte[] value;
+    }
+
+    static final class UpdateResponse {
+    }
+
+    static final class DeleteRequest {
+        byte[] key;
+    }
+
+    static final class DeleteResponse {
+    }
+
+    static abstract class KeyValueServiceImplBase implements BindableService {
+        public abstract void create(
+                KvGson.CreateRequest request, StreamObserver<CreateResponse> responseObserver);
+
+        public abstract void retrieve(KvGson.RetrieveRequest request,
+                                      StreamObserver<KvGson.RetrieveResponse> responseObserver);
+
+        public abstract void update(
+                KvGson.UpdateRequest request, StreamObserver<KvGson.UpdateResponse> responseObserver);
+
+        public abstract void delete(
+                KvGson.DeleteRequest request, StreamObserver<KvGson.DeleteResponse> responseObserver);
+
+        @Override
+        public final ServerServiceDefinition bindService() {
+            ServerServiceDefinition.Builder ssd = ServerServiceDefinition.builder(SERVICE_NAME);
+            ssd.addMethod(CREATE_METHOD, ServerCalls.asyncUnaryCall(
+                    (request, responseObserver) -> create(request, responseObserver)));
+            ssd.addMethod(RETRIEVE_METHOD, ServerCalls.asyncUnaryCall(
+                    (request, responseObserver) -> retrieve(request, responseObserver)));
+            ssd.addMethod(UPDATE_METHOD, ServerCalls.asyncUnaryCall(
+                    (request, responseObserver) -> update(request, responseObserver)));
+            ssd.addMethod(DELETE_METHOD, ServerCalls.asyncUnaryCall(
+                    (request, responseObserver) -> delete(request, responseObserver)));
+            return ssd.build();
+        }
+    }
 }
--- src/main/java/io/grpc/examples/KvService.java
+++ src/main/java/io/grpc/examples/KvService.java
@@ -2,6 +2,7 @@ package io.grpc.examples;
 
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+
 import java.nio.ByteBuffer;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -15,75 +16,75 @@ import java.util.concurrent.TimeUnit;
  */
 final class KvService extends KvGson.KeyValueServiceImplBase {
 
-  private static final long READ_DELAY_MILLIS = 10;
-  private static final long WRITE_DELAY_MILLIS = 50;
+    private static final long READ_DELAY_MILLIS = 10;
+    private static final long WRITE_DELAY_MILLIS = 50;
 
-  private final ConcurrentMap<ByteBuffer, ByteBuffer> store = new ConcurrentHashMap<>();
+    private final ConcurrentMap<ByteBuffer, ByteBuffer> store = new ConcurrentHashMap<>();
 
-  @Override
-  public void create(
-      KvGson.CreateRequest request, StreamObserver<KvGson.CreateResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    ByteBuffer value = ByteBuffer.wrap(request.value);
-    simulateWork(WRITE_DELAY_MILLIS);
-    if (store.putIfAbsent(key, value) == null) {
-      responseObserver.onNext(new KvGson.CreateResponse());
-      responseObserver.onCompleted();
-      return;
+    private static void simulateWork(long millis) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
     }
-    responseObserver.onError(Status.ALREADY_EXISTS.asRuntimeException());
-  }
 
-  @Override
-  public void retrieve(KvGson.RetrieveRequest request,
-      StreamObserver<KvGson.RetrieveResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    simulateWork(READ_DELAY_MILLIS);
-    ByteBuffer value = store.get(key);
-    if (value != null) {
-      KvGson.RetrieveResponse response = new KvGson.RetrieveResponse();
-      response.value = value.array();
-      responseObserver.onNext(response);
-      responseObserver.onCompleted();
-      return;
+    @Override
+    public void create(
+            KvGson.CreateRequest request, StreamObserver<KvGson.CreateResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        ByteBuffer value = ByteBuffer.wrap(request.value);
+        simulateWork(WRITE_DELAY_MILLIS);
+        if (store.putIfAbsent(key, value) == null) {
+            responseObserver.onNext(new KvGson.CreateResponse());
+            responseObserver.onCompleted();
+            return;
+        }
+        responseObserver.onError(Status.ALREADY_EXISTS.asRuntimeException());
     }
-    responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
-  }
 
-  @Override
-  public void update(
-      KvGson.UpdateRequest request, StreamObserver<KvGson.UpdateResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    ByteBuffer newValue = ByteBuffer.wrap(request.value);
-    simulateWork(WRITE_DELAY_MILLIS);
-    ByteBuffer oldValue;
-    do {
-      oldValue = store.get(key);
-      if (oldValue == null) {
+    @Override
+    public void retrieve(KvGson.RetrieveRequest request,
+                         StreamObserver<KvGson.RetrieveResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        simulateWork(READ_DELAY_MILLIS);
+        ByteBuffer value = store.get(key);
+        if (value != null) {
+            KvGson.RetrieveResponse response = new KvGson.RetrieveResponse();
+            response.value = value.array();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            return;
+        }
         responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
-        return;
-      }
-    } while (!store.replace(key, oldValue, newValue));
-    responseObserver.onNext(new KvGson.UpdateResponse());
-    responseObserver.onCompleted();
-  }
-
-  @Override
-  public void delete(
-      KvGson.DeleteRequest request, StreamObserver<KvGson.DeleteResponse> responseObserver) {
-    ByteBuffer key = ByteBuffer.wrap(request.key);
-    simulateWork(WRITE_DELAY_MILLIS);
-    store.remove(key);
-    responseObserver.onNext(new KvGson.DeleteResponse());
-    responseObserver.onCompleted();
-  }
-
-  private static void simulateWork(long millis) {
-    try {
-      TimeUnit.MILLISECONDS.sleep(millis);
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      throw new RuntimeException(e);
     }
-  }
+
+    @Override
+    public void update(
+            KvGson.UpdateRequest request, StreamObserver<KvGson.UpdateResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        ByteBuffer newValue = ByteBuffer.wrap(request.value);
+        simulateWork(WRITE_DELAY_MILLIS);
+        ByteBuffer oldValue;
+        do {
+            oldValue = store.get(key);
+            if (oldValue == null) {
+                responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+                return;
+            }
+        } while (!store.replace(key, oldValue, newValue));
+        responseObserver.onNext(new KvGson.UpdateResponse());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void delete(
+            KvGson.DeleteRequest request, StreamObserver<KvGson.DeleteResponse> responseObserver) {
+        ByteBuffer key = ByteBuffer.wrap(request.key);
+        simulateWork(WRITE_DELAY_MILLIS);
+        store.remove(key);
+        responseObserver.onNext(new KvGson.DeleteResponse());
+        responseObserver.onCompleted();
+    }
 }
